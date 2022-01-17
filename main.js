e95c62b9dd7519a302a534c0e465c7407ff068f3
AndreyAnd7 <92207370+AndreyAnd7@users.noreply.github.com>
2022-01-17 19:49:38 +0200

    56454

---
+++ main.js
@@ -0,0 +1,302 @@
+//let a = prompt('Введите  значення стороны a')
+//let p = a*4
+
+//alert(p)
+
+
+//let a = prompt('Введите  значення стороны a')
+//let s = a*a
+
+//alert(s)
+
+
+//let a = prompt('Введите  значення стороны a')
+//let b = prompt('Введите  значення стороны b')
+//let s = a*b
+//let p = 2*(a+b)
+
+//alert(s)
+//alert(p)
+
+
+//let d = prompt('Введите  значення стороны d')
+//let L = 3.14*d
+
+//alert(L)
+
+
+//let a = prompt('Введите  значення стороны a')
+//let V = a*a*a
+//let s = 6*(a*a)
+
+//alert(V)
+//alert(s)
+
+
+//let a = prompt('Введите  значення стороны a')
+//let b = prompt('Введите  значення стороны b')
+//let c = prompt('Введите  значення стороны c')
+//let V = a*b*c
+//let s = 2*(a*b+b*c+a*c)
+
+//alert(V)
+//alert(s)
+
+
+//let R = prompt('Введите  значення стороны R')
+//let L = 2*3.14*R
+//let s = 3.14*(R*R)
+
+//alert(L)
+//alert(s)
+
+
+//let a = prompt('Введите  значення стороны a')
+//let b = prompt('Введите  значення стороны b')
+//let ar = (a+b)/2
+
+//alert(ar)
+
+
+//let a = prompt('Введите  значення стороны a')
+//let b = prompt('Введите  значення стороны b')
+//let kor = Math.sqrt(a*b)
+
+//alert(kor)
+
+
+//let a = prompt('Введите  значення стороны a')
+//let b = prompt('Введите  значення стороны b')
+//let A = 1*(a+b)
+//let B = a-b
+//let C = a*b
+//let D = Math.sqrt(b)
+//let E = Math.sqrt(a)
+
+//alert(A)
+//alert(B)
+//alert(C)
+//alert(D)
+//alert(E)
+
+
+//let a = prompt('Введите  значення стороны a')
+//let b = prompt('Введите  значення стороны b')
+//let A = 1*(a+b)
+//let B = a-b
+//let C = a*b
+//let D = Math.abs(b)
+//let E = Math.abs(a)
+
+//alert(A)
+//alert(B)
+//alert(C)
+//alert(D)
+//alert(E)
+
+
+//let a = prompt('Введите  значення стороны a')
+//let b = prompt('Введите  значення стороны b')
+//let c = Math.sqrt((a*a)+(b*b))
+//let p = a+b+c
+
+//alert(c)
+//alert(p)
+
+
+//let R1 = prompt('Введите  значення стороны R1')
+//let R2 = prompt('Введите  значення стороны R2')
+//let S1 = 3.14*(R1*R1)
+//let S2 = 3.14*(R2*R2)
+//let S3 = S1-S2
+
+//alert(S1)
+//alert(S2)
+//alert(S3)
+
+
+//let L = prompt('Введите  значення стороны L')
+//let R = L/(2*3.14)
+//let s = 3.14*(R*R)
+
+//alert(R)
+//alert(s)
+
+
+//let S = prompt('Введите  значення стороны L')
+//let D = Math.sqrt)
+//let L = 
+
+//alert(D)
+//alert(L)
+
+
+//let x1 = prompt('Введите  значення стороны x1')
+//let x2 = prompt('Введите  значення стороны x2')
+//let D = Math.abs(x2-x1)
+
+//alert(D)
+
+
+//let A = Number (prompt('Введите  значення стороны A'))
+//let B = Number (prompt('Введите  значення стороны B'))
+//let C = Number (prompt('Введите  значення стороны C'))
+//let AC = A+C 
+//let BC = B+C
+//let ABC = AC+BC
+
+//alert(AC)
+//alert(BC)
+//alert(ABC)
+
+
+//let A = Number (prompt('Введите  значення стороны A'))
+//let C = Number (prompt('Введите  значення стороны C'))
+//let B = Number (prompt('Введите  значення стороны B'))
+//let AC = A+C  
+//let BC = B+C
+//let ABC = AC*BC
+
+//alert(AC)
+//alert(BC)
+//alert(ABC)
+
+
+//let x1 = Number (prompt('Введите  значення стороны x1'))
+//let x2 = Number (prompt('Введите  значення стороны x2'))
+//let y1 = Number (prompt('Введите  значення стороны y1'))
+//let y2 = Number (prompt('Введите  значення стороны y2'))
+//let P = 2*(Math.abs(x1-x2)+Math.abs(y1-y2))  
+//let S = Math.abs(x1-x2)*Math.abs(y1-y2)
+
+//alert(P)
+//alert(S)
+
+
+//let x1 = prompt('Введите  значення стороны x1')
+//let x2 = prompt('Введите  значення стороны x2')
+//let y1 = prompt('Введите  значення стороны y1')
+//let y2 = prompt('Введите  значення стороны y1')
+//let P =  Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2)) 
+
+//alert(P)
+
+
+//let L = Number (prompt('Введите  значення стороны L'))
+//let M = Math.floor(L/100)
+
+//alert(M)
+
+
+//let M = Number (prompt('Введите  значення стороны M'))
+//let T = Math.floor(M/1000)
+
+//alert(T)
+
+
+//let B = Number (prompt('Введите  значення стороны B'))
+//let K = Math.floor(B/1024)
+
+//alert(K)
+
+
+//let A = prompt('Введите  значення A, A>B A>0')
+//let B = prompt('Введите  значення B, A>B A>0')
+//let К = Math.floor(A/B)
+
+//alert(K)
+
+
+//let A = prompt('Введите  значення A, A>B A>0')
+//let B = prompt('Введите  значення B, A>B A>0')
+//let К = A%B
+
+//alert(K)
+
+
+//let A = prompt('Введите двузначно число')
+//let t = A/10
+//let u = A%10    
+
+//alert(t)
+//alert(u)
+
+
+//let A = prompt('Введите двузначно число')
+//let t = (A/10)+(A%10)
+//let u = (A%10)*(A/10)    
+
+//alert(t)
+//alert(u)
+
+
+//let AB = prompt('Введите двузначно число')
+//let BA = AB/10+AB%10*10    
+
+//alert(BA)
+
+
+//let A = prompt('Введите трехзначно число')
+//let B = A/100    
+
+//alert(B)
+
+
+//let A = prompt('Введите трехзначно число')
+//let B = A%10
+//let C = A%100/10
+
+//alert(B)
+//alert(C)
+
+
+//let A = prompt('Введите трехзначно число')
+//let B = A%10
+//let C = A%100/10
+//let D = A/100
+//let E = B+C+D
+//let G =B*C*D
+
+//alert(B)
+//alert(C)
+//alert(D)
+//alert(E)
+//alert(G)
+
+
+//let A = prompt('Введите двузначно число')
+//let B = (A%10)*100+(A%100/10)*10+(A/100)
+
+//alert(B)
+
+
+//let A = prompt('Введите двузначно число')
+//let B = (A%100/10*100)+(A%10)*10+A/100
+
+//alert(B)
+
+
+//let A = prompt('Введите двузначно число')
+//let C = (A%10)*100+(A/100)*10+(A%100/10)
+
+//alert(C)
+
+
+//let A = prompt('Введите двузначно число')
+//let B = A/100 
+//let c = (A/10)%10
+//let d = A%10
+//let e = c*100+B*10+d
+
+//alert(B)
+//alert(c)
+//alert(d)
+//alert(e)
+
+
+//let A = prompt('Введите двузначно число')
+//let B = (A/100)*100+(A%10)*10+(A%100/10)
+
+//alert(B)
+
+
